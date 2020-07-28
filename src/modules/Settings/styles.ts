f37ec498915c848b4818f9039bc50ab6ece551a5
kulek <ezsins@outlook.com>
2020-07-28 10:31:54 +0200

    style: fix modal on mobile

--- src/modules/Settings/styles.ts
+++ src/modules/Settings/styles.ts
@@ -1,6 +1,16 @@
 import styled from 'styled-components/macro';
 
 export const ModalContainer = styled.div`
-  width: 600px;
+  width: 100%;
   text-align: center;
+
+  @media screen and (min-width: 300px) {
+    width: 250px;
+  }
+  @media screen and (min-width: 600px) {
+    width: 400px;
+  }
+  @media screen and (min-width: 900px) {
+    width: 600px;
+  }
 `;
